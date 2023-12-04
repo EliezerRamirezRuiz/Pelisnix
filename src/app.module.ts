399ceeef33cb53d50172d78630f3d35697cd4bdb
EliezerRamirezRuiz <ramirez.ruiz.eliezer.reuven@gmail.com>
2023-12-04 01:06:15 -0300

    Add movie module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,13 +9,16 @@ import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthService } from './auth/auth.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { MovieService } from './movie/movie.service';
+import { MovieController } from './movie/movie.controller';
+import { MovieModule } from './movie/movie.module';
 
 
 
 @Module({
-  imports: [AuthModule, PrismaModule],
-  controllers: [AppController, AuthController],
-  providers: [AppService, AuthService, PrismaService],
+  imports: [AuthModule, PrismaModule, MovieModule],
+  controllers: [AppController, AuthController, MovieController],
+  providers: [AppService, AuthService, PrismaService, MovieService],
 })
 export class AppModule {
   
