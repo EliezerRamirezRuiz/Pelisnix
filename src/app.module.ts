79263084c57928de723f19bc5f428b0ce91ab41f
EliezerRamirezRuiz <ramirez.ruiz.eliezer.reuven@gmail.com>
2023-12-04 03:12:34 -0300

    Add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,37 @@ import { Module } from '@nestjs/common';
 // module
 import { AuthModule } from './auth/auth.module';
 // controller
-import { AppController } from './app.controller';
 import { AuthController } from './auth/auth.controller';
 // service
-import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { AuthService } from './auth/auth.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { MovieService } from './movie/movie.service';
 import { MovieController } from './movie/movie.controller';
 import { MovieModule } from './movie/movie.module';
+import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
+import { UserModule } from './user/user.module';
 
 
 
 @Module({
-  imports: [AuthModule, PrismaModule, MovieModule],
-  controllers: [AppController, AuthController, MovieController],
-  providers: [AppService, AuthService, PrismaService, MovieService],
+  imports: [
+    AuthModule, 
+    PrismaModule, 
+    MovieModule, 
+    UserModule
+  ],
+  controllers: [
+    AuthController, 
+    MovieController, 
+    UserController
+  ],
+  providers: [
+    AuthService, 
+    PrismaService, 
+    MovieService, 
+    UserService],
 })
 export class AppModule {
   
